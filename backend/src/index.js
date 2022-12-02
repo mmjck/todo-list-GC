f3b3e9f940700c401d7d89d9502ae842c2dc7c38
Jackson Matheus <msantosjck@gmail.com>
2022-12-02 19:14:37 -0400

    fix: morgan

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,17 @@
 import express from "express";
+import morgan from 'morgan';
 import routes from "./routes"
+
 import middlewares from './middlewares'
 const app = express();
+const logger = morgan('combined')
 
 const PORT = process.env.PORT || 3333;
 
 app.use(routes);
 app.use(middlewares("tiny"));
+app.use(logger)
+
 
 app.listen(PORT, () => {
     console.log(`Server running on port: ${PORT}`);
