7343ee2bd8703590b14c2f9ef444c9b644395944
Jackson Matheus <msantosjck@gmail.com>
2022-12-02 19:10:01 -0400

    feat: added middlewares and routes file

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,15 +1,12 @@
 import express from "express";
-
+import routes from "./routes"
+import middlewares from './middlewares'
 const app = express();
-
 
 const PORT = process.env.PORT || 3333;
 
-
-app.get("/", (req, res) => {
-    res.send("Hello World Back")
-});
-
+app.use(routes);
+app.use(middlewares("tiny"));
 
 app.listen(PORT, () => {
     console.log(`Server running on port: ${PORT}`);
