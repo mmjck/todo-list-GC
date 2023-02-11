a2df998e11883f989df4ba948436186c253f2f53
Rodrigo Taveira <rodrigotaveiraa@pop-os.localdomain>
2023-02-11 15:32:37 -0400

    update structure front and global styles

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,10 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import TodoList from "./components/TodoList";
 import Container from "react-bootstrap/Container";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import TodoCreate from "./components/TodoCreate/";
+import TodoDetails from "./components/TodoDetails";
+import Sobre from "./components/Sobre";
 
 function App() {
   return (
@@ -10,6 +13,9 @@ function App() {
       <Container>
         <Routes>
           <Route path="/" element={<TodoList />}></Route>
+          <Route path="/create" element={<TodoCreate />}></Route>
+          <Route path="/todo/:id" element={<TodoDetails />}></Route>
+          <Route path="/sobre" element={<Sobre />}></Route>
         </Routes>
       </Container>
     </BrowserRouter>
