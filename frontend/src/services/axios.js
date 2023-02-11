394e9b6c204ba5fa30bcca38b6bd2ab5cfeefca7
Rodrigo Taveira <rodrigotaveiraa@pop-os.localdomain>
2023-02-11 15:28:07 -0400

    create service folder

--- frontend/src/services/axios.js
+++ frontend/src/services/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: `http://localhost:${process.env.PORT_BACK}`,
+  baseURL: `http://localhost:${process.env.REACT_APP_PORT_BACK}`,
 });
-
+console.log(process.env.REACT_APP_PORT_BACK);
 export default api;
