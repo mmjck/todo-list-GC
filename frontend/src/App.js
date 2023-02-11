1508c184f4de07416981556714fd00c1e6ab146e
Rodrigo Taveira <rodrigotaveiraa@pop-os.localdomain>
2023-02-10 21:59:12 -0400

    add react-router-dom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,19 @@
+import Header from "./components/Header";
+import TodoList from "./components/TodoList";
+import Container from "react-bootstrap/Container";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 function App() {
-  return <h1>Hello world</h1>;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Container>
+        <Routes>
+          <Route path="/" element={<TodoList />}></Route>
+        </Routes>
+      </Container>
+    </BrowserRouter>
+  );
 }
 
 export default App;
